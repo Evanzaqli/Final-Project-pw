e64449fcc6530865c1684fe9bf4c3d54d6e4467b
Evan Zaqli <evanzaqli1@gmail.com>
2024-05-16 22:10:55 +0700

    hampir selesai

---
+++ asset/app.js
@@ -0,0 +1,20 @@
+/* Created by Tivotal */
+
+let swiper = new Swiper(".wrapper", {
+    loop: true,
+    autoplay: true,
+    autoplayTimeout: 2000,
+    autoplayHoverPause: true,
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    coverflowEffect: {
+      rotate: 0,
+      stretch: 0,
+      depth: 200,
+      modifier: 1,
+      slideShadows: false,
+    },
+  });
+  
