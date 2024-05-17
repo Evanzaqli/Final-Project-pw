3a30a1f1e54fc3d2a411ff2d1a3eb0a59c48e6d8
Evan Zaqli <evanzaqli1@gmail.com>
2024-05-17 10:56:56 +0700

    selesai

--- asset/app.js
+++ asset/app.js
@@ -1,4 +1,4 @@
-/* Created by Tivotal */
+/* js swiper */
 
 let swiper = new Swiper(".wrapper", {
     loop: true,
@@ -17,4 +17,24 @@ let swiper = new Swiper(".wrapper", {
       slideShadows: false,
     },
   });
+
+  /*js login */
+  document.getElementById('loginForm').addEventListener('submit', function(event) {
+    event.preventDefault(); // Prevent form from submitting
+
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+    const errorMessage = document.getElementById('error-message');
+
+    // Simple authentication logic (for demonstration purposes)
+    const validEmail = 'user@gmail.com';
+    const validPassword = 'user';
+
+    if (email === validEmail && password === validPassword) {
+        // Redirect to another page or perform other actions here
+        window.location.href = 'index.html'; // Change this to the desired URL
+    } else {
+        errorMessage.textContent = 'Invalid email or password';
+    }
+});
   
